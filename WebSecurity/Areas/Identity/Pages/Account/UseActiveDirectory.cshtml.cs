9b6a10db8efba86617239c115ea3126d9f82bbb8
Aliaksei Mukhamiadziyeu <Aliaksei_Mukhamiadziyeu@epam.com>
2022-08-04 17:30:56 +0400

    Task 2: add UseActiveDirectory page

---
+++ WebSecurity/Areas/Identity/Pages/Account/UseActiveDirectory.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebSecurity.Areas.Identity.Pages.Account
+{
+    public class UseActiveDirectoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
